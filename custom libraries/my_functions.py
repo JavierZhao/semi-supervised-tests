4c2d958d1901aa6d1b0db45ed86b44db4dbdad7c
Javier Zhao <zhaoziha@usc.edu>
2022-08-29 11:50:14 -0700

    included custom libraries

---
+++ custom libraries/my_functions.py
@@ -0,0 +1,107 @@
+def calculate_tpr_fpr(y_real, y_pred):
+    '''
+    Calculates the True Positive Rate (tpr) and the True Negative Rate (fpr) based on real and predicted observations
+    
+    Args:
+        y_real: The list or series with the real classes
+        y_pred: The list or series with the predicted classes
+        
+    Returns:
+        tpr: The True Positive Rate of the classifier
+        fpr: The False Positive Rate of the classifier
+    '''
+    
+    # Calculates the confusion matrix and recover each element
+    cm = confusion_matrix(y_real, y_pred)
+    TN = cm[0, 0]
+    FP = cm[0, 1]
+    FN = cm[1, 0]
+    TP = cm[1, 1]
+    
+    # Calculates tpr and fpr
+    tpr =  TP/(TP + FN) # sensitivity - true positive rate
+    fpr = 1 - TN/(TN+FP) # 1-specificity - false positive rate
+    
+    return tpr, fpr
+    
+def get_all_roc_coordinates(y_real, y_proba):
+    '''
+    Calculates all the ROC Curve coordinates (tpr and fpr) by considering each point as a threshold for the predicion of the class.
+    
+    Args:
+        y_real: The list or series with the real classes.
+        y_proba: The array with the probabilities for each class, obtained by using the `.predict_proba()` method.
+        
+    Returns:
+        tpr_list: The list of TPRs representing each threshold.
+        fpr_list: The list of FPRs representing each threshold.
+    '''
+    tpr_list = [0]
+    fpr_list = [0]
+    resolution = 50
+    
+    for i in range(resolution):
+        threshold = i / resolution
+#         threshold = y_proba[i]
+        y_pred = y_proba >= threshold
+        tpr, fpr = calculate_tpr_fpr(y_real, y_pred)
+        tpr_list.append(tpr)
+        fpr_list.append(fpr)
+        
+    return tpr_list, fpr_list
+
+def plot_roc_curve(tpr, fpr, scatter = True, ax = None):
+    '''
+    Plots the ROC Curve by using the list of coordinates (tpr and fpr).
+    
+    Args:
+        tpr: The list of TPRs representing each coordinate.
+        fpr: The list of FPRs representing each coordinate.
+        scatter: When True, the points used on the calculation will be plotted with the line (default = True).
+    '''
+    if ax == None:
+        plt.figure(figsize = (5, 5))
+        ax = plt.axes()
+    
+    if scatter:
+        sns.scatterplot(x = fpr, y = tpr, ax = ax)
+    sns.lineplot(x = fpr, y = tpr, ax = ax)
+    sns.lineplot(x = [0, 1], y = [0, 1], color = 'green', ax = ax)
+    plt.xlim(-0.05, 1.05)
+    plt.ylim(-0.05, 1.05)
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    
+def plot_overlayed_roc_curve(classes, labels, predictions, ax = None, figsize=(12, 6), ncol=2):
+    '''
+    Plots overlayed ROC curves.
+    
+    Args:
+        classes: The classes used in classification
+        labels: The list of labels.
+        predictions: The list of predicted classes. First dimension should match with the dimension of labels
+    '''
+    assert labels.size() == predictions[:, 0].size()
+    if predictions.type() == 'torch.cuda.FloatTensor':
+        predictions = predictions.cpu()
+        
+    if ax == None:
+        fig, ax = plt.subplots(figsize=(12, 6))
+    
+    for i in range(len(classes)):
+        c = classes[i]
+        y_real = [1 if y == c else 0 for y in labels]
+        y_proba = predictions[:, i]
+        tpr, fpr = get_all_roc_coordinates(y_real, y_proba)
+        ax.plot(fpr, tpr, label = c)
+
+    x = np.linspace(0, 1, 10)
+    Y = x
+    plt.plot(x, Y, color='g')
+    plt.xlim(-0.05, 1.05)
+    plt.ylim(-0.05, 1.05)
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    ax.legend(loc='best', bbox_to_anchor=(0.5, -0.20), shadow=False, ncol=ncol)
+    plt.title("ROC Curve OvR")
+    plt.show()
