dc1a497cb64ea934665457f7df53c034aa46ed1e
Javier Zhao <zhaoziha@usc.edu>
2022-08-29 12:10:24 -0700

    Update my_functions.py

--- custom_libraries/my_functions.py
+++ custom_libraries/my_functions.py
@@ -1,3 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_auc_score
+
+
+
 # To plot pretty figures
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -66,8 +77,6 @@ def plot_roc_curve(tpr, fpr, scatter = True, ax = None):
         fpr: The list of FPRs representing each coordinate.
         scatter: When True, the points used on the calculation will be plotted with the line (default = True).
     '''
-    import matplotlib as mpl
-    import matplotlib.pyplot as plt
     if ax == None:
         plt.figure(figsize = (5, 5))
         ax = plt.axes()
@@ -90,9 +99,6 @@ def plot_overlayed_roc_curve(classes, labels, predictions, ax = None, figsize=(1
         labels: The list of labels.
         predictions: The list of predicted classes. First dimension should match with the dimension of labels
     '''
-    import matplotlib as mpl
-    import matplotlib.pyplot as plt
-    
     assert labels.size() == predictions[:, 0].size()
     if predictions.type() == 'torch.cuda.FloatTensor':
         predictions = predictions.cpu()
